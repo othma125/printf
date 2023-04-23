c28470e2a8bb45a97b5bf01d0ad9e88d42a6262c
othmane <othma125@hotmail.com>
2023-04-23 21:49:26 +0000

    merche third commit

--- print_rot13.c
+++ print_rot13.c
@@ -1,33 +1,6 @@
 #include "main.h"
 
 /**
- * rot13 - check the code
- * @s: input string
- * Return: string
- */
-char *rot13(char *s)
-{
-	int i = 0, j;
-	char *inputs = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char *outputs = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
-	while (s[i] != '\0')
-	{
-		for (j = 0; inputs[j] != '\0'; j++)
-		{
-			if (s[i] == inputs[j])
-			{
-				s[i] = outputs[j];
-				break;
-			}
-		}
-		i++;
-	}
-	return (s);
-}
-#include "main.h"
-
-/**
  * rot13_recursion - check code
  * @s: string to display
  * Return: string length
