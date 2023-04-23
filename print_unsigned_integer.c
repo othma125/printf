2c3d96f93e67e0dea3970a4e4bc3ce0b17cda55c
othmane <othma125@hotmail.com>
2023-04-23 17:44:58 +0000

    8th commit othmane

--- print_unsigned_integer.c
+++ print_unsigned_integer.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_unsigned - print numbers in decimal
+ * print_unsigned_ - print numbers in decimal
  * @n: input number
  * Return: string length
  */
-int print_unsigned(unsigned int n)
+int print_unsigned_(unsigned int n)
 {
 	if (n >= 10)
-		return (print_unsigned(n / 10) + print_unsigned(n % 10));
+		return (print_unsigned_(n / 10) + print_unsigned_(n % 10));
 	_putchar('0' + n);
 	return (1);
 }
@@ -19,5 +19,5 @@ int print_unsigned(unsigned int n)
  */
 int print_unsigned_integer(va_list va)
 {
-	return (print_unsigned(va_arg(va, unsigned int)));
+	return (print_unsigned_(va_arg(va, unsigned int)));
 }
