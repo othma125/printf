01eaf19e0435e7f480b8ac078256c7e8465b612a
othmane <othma125@hotmail.com>
2023-04-24 15:08:59 +0000

    print char update

--- print_char.c
+++ print_char.c
@@ -7,7 +7,9 @@
  */
 int print_char(va_list va)
 {
-	char c = va_arg(va, int);
+	char buffer[2];
 
-	return (write(1, &c, 1));
+	buffer[0] = va_arg(va, int);
+	buffer[1] = '\0';
+	return (write(1, buffer, 1));
 }
