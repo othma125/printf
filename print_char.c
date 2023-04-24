e879c18801fa3a5d96d9abe089a548052d82a713
othmane <othma125@hotmail.com>
2023-04-24 12:29:31 +0000

    print char fixing

--- print_char.c
+++ print_char.c
@@ -7,7 +7,10 @@
  */
 int print_char(va_list va)
 {
-	char c = va_arg(va, int);
+	char c = va_arg(va, int), buffer[2];
+	int i = 0;
 
-	return (write(1, &c, 1));
+	buffer[i++] = c;
+	buffer[i] = '\0';
+	return (write(1, buffer, 1));
 }
