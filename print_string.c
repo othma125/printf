f082e9ce3e21a6953dbe4d0ec2e0e7c7ac486f55
othmane <othma125@hotmail.com>
2023-04-25 14:42:28 +0000

    print string modification

--- print_string.c
+++ print_string.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_string - check code
  * @va: variadic list
@@ -7,11 +7,16 @@
  */
 int print_string(va_list va)
 {
-	char *str = va_arg(va, char*);
+	char *str;
 	int len = 0;
 
+	str = va_arg(va, char*);
 	str = str == NULL ? "(null)" : str;
 	while (*(str + len) != '\0')
-		len++;
+	{
+		if (str[len] > 0 && (str[len] < 32 || str[len] >= 127 || str[len] == '\n'))
+			len++;
+		return (-1);
+	}
 	return (write(1, str, len));
 }
